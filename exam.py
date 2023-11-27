e237f37d7997ab8b47626f68f165cb03af3a27cb
choi seong woo <cswcjw311427@naver.com>
2023-11-27 10:41:52 +0900

    try to exam problems.

--- exam.py
+++ exam.py
@@ -1,5 +1,75 @@
-arr = [1, 2, 3]
-try:
-  print(arr[3])
-except:
- print("리스트의 요소에 접근하지 못했습니다.")
+# arr = [1, 2, 3]
+# try:
+#   print(arr[3])
+# except:
+#  print("리스트의 요소에 접근하지 못했습니다.")
+
+# nums = list(range(0,10,3))
+# print(nums)
+
+# list_a = ["가", "나", "다", "라", "마"]
+# for out_list_a in list_a[2:5]:
+#   print(out_list_a)
+
+# list_a = [0,1,2,3]
+# list_a.append("")
+# print(list_a)
+
+# list_a = [10,20,30,40,50]
+# list_a.clear()
+# print(list_a)
+
+# list_a = [2]
+# list_b = []
+# for list_a_o in list_a:
+#   for list_b_o in list_b:
+#     print(list_b_o)
+
+# import math
+# pi = 3.14152926535
+# print(round(pi,8))
+
+# name_dic = {"name1":"이승헌", "name2":"이미자"}
+# for key in name_dic:
+#   print(name_dic[key])
+
+# list_a = [0,1,2,3,4,5]
+# sum = 0
+# for i in range(len(list_a)):
+#   sum = sum+i
+# print(sum)
+
+# def fn(a, b, c, d, e):
+#   print(a, b, c, d, 
+# fn("a", "b", "c" , "b" , "b")
+
+# def hello(msg="안녕하세요"):
+#   print(msg + "!")
+# hello()
+
+# def fn(a, b=[]):
+#   b.append(a)
+#   print(b)
+# fn(9)
+
+# def sum(*numbers):
+#   sum_value = 0
+#   for number in numbers:
+#     sum_value = sum_value + number
+#   return sum_value
+
+# result = sum(1,3)
+# print("1 + 3 =", result)
+# print("1 + 3 + 5 + 7 =", sum(1,3,5,7))
+
+# with open("basic.txt","w") as file:
+#   file.write("Hello")
+# with open("basic.txt", "r") as file:
+#   contents = file.read()
+# print(contents)
+
+# arr = [1, 2, 3]
+# try:
+#   print(arr[3])
+# except:
+#  print("리스트의 요소에 접근하지 못했습니다.")
